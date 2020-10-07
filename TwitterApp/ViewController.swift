9a7daa32e91968a6332d9311146bbfdf1cba773c
Kingsley Ikechukwu <kingsley.ikechukwu@lc.cuny.edu>
2020-10-07 19:35:59 -0400

    Initial Commit

---
+++ TwitterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterApp
+//
+//  Created by Kingsley Ikechukwu on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
